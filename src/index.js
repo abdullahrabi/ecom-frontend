1ffe37d729fefacefd8b7e06aae2d86562b1445b
Abdullah Rabi <abdullahrabi026@gmail.com>
2025-08-22 02:34:40 +0500

    readjusting code

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
-import CaptchaProvider from './Context/CaptchaContext';  // ✅ use Provider
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <CaptchaProvider>
+  
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </CaptchaProvider>
+
 );
 
 reportWebVitals();
