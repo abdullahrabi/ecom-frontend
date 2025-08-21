06bb324a7a7b7f633633bd3eb6f672be5d885507
Abdullah Rabi <abdullahrabi026@gmail.com>
2025-08-22 04:03:05 +0500

    Testing 5

--- src/Components/CaptchaModel/CaptchaModel.jsx
+++ src/Components/CaptchaModel/CaptchaModel.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Turnstile from "react-turnstile";
-import './CaptchaModel';
+
 
 const CaptchaModal = ({ onVerify, onClose }) => {
   // disable scroll when modal opens
