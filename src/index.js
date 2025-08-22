43c310e22156f6d57fcfb61a454064cba784d93b
Abdullah Rabi <abdullahrabi026@gmail.com>
2025-08-22 05:15:55 +0500

    Reverting changes

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
-import CaptchaModal from '../Components/CaptchaModel/CaptchaModel.jsx';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <CaptchaModal>
+  
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-</CaptchaModal>
+
 );
 
 reportWebVitals();
